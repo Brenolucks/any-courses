1b3eea2b67c38e14075b3e1604c730bd88611e1a
brenolucks <brenolucas0106@gmail.com>
2025-05-12 20:48:16 -0300

    first commit

---
+++ src/main/java/brenolucks/anycourses/AnycoursesApplication.java
@@ -0,0 +1,13 @@
+package brenolucks.anycourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnycoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnycoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/brenolucks/anycourses/AnycoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package brenolucks.anycourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnycoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
